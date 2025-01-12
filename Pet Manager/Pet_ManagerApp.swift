1c4ebf0fa6f3b443b50d39c19f57777bb862f6fd
leondaki <leondaki@gmail.com>
2025-01-12 18:53:57 -0500

    Initial Commit

---
+++ Pet Manager/Pet_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_ManagerApp.swift
+//  Pet Manager
+//
+//  Created by Leonidas Kalpaxis on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Pet_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
