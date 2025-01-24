d99436376b5e34aedd9dba3286e131e0f93a31ba
leondaki <leondaki@gmail.com>
2025-01-23 23:09:44 -0500

    add custom tab bar

---
+++ CustomTabBar.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTabBar.swift
+//  Pet Manager
+//
+//  Created by Leonidas Kalpaxis on 1/23/25.
+//
+
+import Foundation
---
+++ Pet Manager/AddPetsView.swift
@@ -0,0 +1,8 @@
+//
+//  AddPetsView.swift
+//  Pet Manager
+//
+//  Created by Leonidas Kalpaxis on 1/23/25.
+//
+
+import Foundation
